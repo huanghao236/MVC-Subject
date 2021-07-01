65cbebd2daafadcafdad0876c4d114715c14bf24
huanghao <1915392198@qq.com>
2021-07-01 17:19:28 +0800

    修改

--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@ namespace Hao;
 ini_set("display_errors","On");
 //composer自动加载
 require '../vendor/autoload.php';
-// 加载基础文件
-require '../src/lib/base.php';
 //初始化
 $http = (new App())->http;
 $response = $http->run();
