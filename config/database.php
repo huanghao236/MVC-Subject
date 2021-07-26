9464e9cf4fe6ea958c1d5526ef644ec20bcdc0a2
huanghao <1915392198@qq.com>
2021-07-26 11:20:13 +0800

    修改

--- config/database.php
+++ config/database.php
@@ -4,9 +4,37 @@
  * 数据库、noSql配置文件
  */
 return [
+
+    //所选连接的数据库类型
+    'default' => env('DB_CONNECTION','mysql'),
+
     'connections' => [
         'mysql' => [
-            'driver' => 'mysql'
-        ]
+            // 数据库类型
+            'type'            => env('type', 'mysql'),
+            // 服务器地址
+            'hostname'        => env('hostname', '127.0.0.1'),
+            // 数据库名
+            'database'        => env('database', ''),
+            // 用户名
+            'username'        => env('username', 'root'),
+            // 密码
+            'password'        => env('password', ''),
+            // 端口
+            'hostport'        => env('hostport', '3306'),
+            // 数据库连接参数
+            'params'          => [],
+            // 数据库编码默认采用utf8
+            'charset'         => 'utf8',
+            'collation'       => 'utf8_unicode_ci',
+            // 数据库表前缀
+            'prefix'          => env('prefix', ''),
+            // 是否严格检查字段是否存在
+            'fields_strict'   => true,
+            // 是否需要断线重连
+            'break_reconnect' => false,
+            // 开启字段缓存
+            'fields_cache'    => false,
+        ],
     ]
 ];
